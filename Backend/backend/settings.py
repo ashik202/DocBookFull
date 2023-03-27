b315b7b20fc7bfa821b6937fdc5b16e3a554d6ee
Ashik S <ashikparayady@gmail.com>
2023-03-27 15:20:44 +0530

    host update 2

--- Backend/backend/settings.py
+++ Backend/backend/settings.py
@@ -186,8 +186,8 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # media file
-MEDIA_URL = os.path.join(os.path.dirname(BASE_DIR),'media')
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
